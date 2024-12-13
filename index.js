f5072d9eeed4cad2892964dd4f386ba0a7327740
HaroldCarson <mair0610@gmail.com>
2024-12-13 12:41:20 -0500

    divisible by 5

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ let n3 = 20;
 let n4 = 5;
 
 //Check if all numbers are divisible by 5
+let allDivisibleByFive = (n1 % 5 === 0) && (n2 % 5 === 0) && (n3 % 5 === 0) && (n4 % 5 === 0);
+console.log(`Are all numbers divisible by 5? ${allDivisibleByFive}`);
