19772ccf271d21e29c36d1f1a72c574439623116
HaroldCarson <mair0610@gmail.com>
2024-12-14 12:48:28 -0500

    trip B and C

--- index.js
+++ index.js
@@ -25,12 +25,36 @@ console.log(`Is the first number greater than 25? ${isGreaterThan25}`);
 //How many gallons of fuel is needed for the trip
 //What is the cost of the fuel for the trip
 //How long will the trip take
+
+//Trip A
 let totalDistance = 1500;
 let milesPerGallonA = 30;
 let speedA = 55;
 let gallonsNeededA = totalDistance / milesPerGallonA;
 let fuelCostA = gallonsNeededA * 3.00;
 let tripDurationA = totalDistance / speedA;
-console.log(`Gallons needed for the trip: ${gallonsNeededA}`);//=50 gallons
-console.log(`Cost of the fuel for the trip: ${fuelCostA}`);//=150
-console.log(`Trip duration: ${tripDurationA}`);//=27.27 hours 
+//Trip B
+let milesPerGallonB = 28;
+let speedB = 60;
+let gallonsNeededB = totalDistance / milesPerGallonB;
+let fuelCostB = gallonsNeededB * 3.00;
+let tripDurationB = totalDistance / speedB;
+//Trip C
+let milesPerGallonC = 23;
+let speedC = 75;
+let gallonsNeededC = totalDistance / milesPerGallonC;
+let fuelCostC = gallonsNeededC * 3.00;
+let tripDurationC = totalDistance / speedC; 
+
+//log trip A
+console.log(`Gallons needed for the trip: ${gallonsNeededB}`);//=50 gallons
+console.log(`Cost of the fuel for the trip: ${fuelCostB}`);//=$150 fuel cost
+console.log(`Trip duration: ${tripDurationB}`);//=27.27 hour trip
+//log trip B
+console.log(`Gallons needed for the trip: ${gallonsNeededA}`);//=53.57 gallons
+console.log(`Cost of the fuel for the trip: ${fuelCostA}`);//=$160.71 fuel cost
+console.log(`Trip duration: ${tripDurationA}`);//=25 hour trip
+//log trip C
+console.log(`Gallons needed for the trip: ${gallonsNeededC}`);//=65.21 gallons
+console.log(`Cost of the fuel for the trip: ${fuelCostC}`);//=$195.63 fuel cost
+console.log(`Trip duration: ${tripDurationC}`);//=20 hour trip  
